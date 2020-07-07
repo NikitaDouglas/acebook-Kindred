f225f528de83b61115131f861a2c4363e44a583c
Sophie Worsnop <sophie.g.worsnop@gmail.com>
2020-07-07 22:16:27 +0100

    add basic bootstrap/css

--- spec/features/user_can_like_post_spec.rb
+++ spec/features/user_can_like_post_spec.rb
@@ -1,18 +1,19 @@
-feature "User can like a post" do
-  scenario "user sees post" do
+# frozen_string_literal: true
+
+feature 'User can like a post' do
+  scenario 'user sees post' do
     visit('/')
     sign_up
     p add_post_helper
     visit '/posts'
-    expect(page).to have_content("My first post")
+    expect(page).to have_content('My first post')
   end
 
-  scenario "user can click <3" do
+  scenario 'user can click <3' do
     visit('/')
     sign_up
     add_post_helper
     visit '/posts'
     expect(page).to have_button('<3')
   end
-
 end
