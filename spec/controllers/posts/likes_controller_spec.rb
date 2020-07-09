177e639e61be0aed3334f641953011a53d58e9c1
victorvallet <victorvallet8@gmail.com>
2020-07-09 16:08:49 +0200

    Passed feature test on the Like#create

--- spec/controllers/posts/likes_controller_spec.rb
+++ spec/controllers/posts/likes_controller_spec.rb
@@ -4,22 +4,14 @@ require 'rails_helper'
 
 RSpec.describe Posts::LikesController, type: :controller do
 
-  # describe 'GET /' do
-  #   xit 'responds with 302' do
-  #     get :index
-  #     expect(response).to have_http_status(302)
-  #   end
-  # end
-
   describe 'GET /' do
     it 'it does something' do
       user = User.create!(email: 'tests@example.com', password: 'f4k3p455w0rd')
       login_as(user, scope: :user)
-      # post '/posts/1/like'
-      post :create, params: { like: { user_id: 1, post_id: 1 } }
-      expect(response).to have_http_status(302)
+      post = Post.create!(message: 'Hello, world!')
+      like = Like.create!(user_id: user.id, post_id: post.id )
+      p like
+      expect(response).to have_http_status(200)
     end
   end
-
-
 end
