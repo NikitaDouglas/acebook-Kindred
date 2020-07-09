3e15ebba25a8dd95223c53a3c3e6a877bd531a91
Sophie Worsnop <sophie.g.worsnop@gmail.com>
2020-07-09 15:53:12 +0100

    Fixed failing tests

--- spec/helpers/add_post_helper_spec.rb
+++ spec/helpers/add_post_helper_spec.rb
@@ -1,6 +1,15 @@
 # frozen_string_literal: true
 
+# def add_post_helper
+#  p  Post.new
+#  p Post.create(id: 2, message: "Hello world!", created_at: "2020-07-09 14:44:28", updated_at: "2020-07-09 14:44:28", name: "Sophie", user_id: 1)
+# end
+
 def add_post_helper
-  Post.new
-  Post.create(message: 'My first post')
+    visit('/posts')
+    click_link 'Write a post...'
+    fill_in 'Name', with: 'Sophie'
+    fill_in 'Message', with: 'Hello world!'
+    click_button 'Submit'
 end
+
