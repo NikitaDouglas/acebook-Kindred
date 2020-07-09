b45a7d206b643764738006ef9a0828cc6e6c1a78
Tim <TimCPB@github.com>
2020-07-09 17:54:43 +0100

    failing 'creates a like' unit test in likes_controller_spec.rb

--- spec/controllers/posts/likes_controller_spec.rb
+++ spec/controllers/posts/likes_controller_spec.rb
@@ -10,8 +10,24 @@ RSpec.describe Posts::LikesController, type: :controller do
       login_as(user, scope: :user)
       post = Post.create!(message: 'Hello, world!')
       like = Like.create!(user_id: user.id, post_id: post.id )
-      p like
       expect(response).to have_http_status(200)
     end
   end
+
+  # describe 'POST /' do
+  #   it 'creates a like' do
+  #     user = User.create!(email: 'tests@example.com', password: 'f4k3p455w0rd')
+  #     login_as(user, scope: :user)
+  #     post = Post.create!(message: 'Hello, world!')
+  #     like = Like.create!(user_id: user.id, post_id: post.id )
+
+  #     post :create, params: { user_id: user.id, post_id: post.id }
+  #     # get :index, params: { company_id: @company_1.id } –
+
+  #     # expect(response).to have_http_status(200)
+  #     p Like.find_by(post_id: post.id)
+  #     expect(Like.find_by(post_id: post.id)).to be
+  #   end
+  # end
+
 end
